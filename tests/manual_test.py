97b078c56470cbcdb7d649c8e8d86845854c79e8
Adam Peachey <u2101940@domain>
2025-01-23 19:22:55 +0000

    fix tests

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -1,30 +1,30 @@
 import gym
+import sys
+sys.path.append("..")
 import gym_snake
 
-if __name__=="__main__":
-    print("PRESS q to quit")
-    print("wasd to move, f to press")
-    env = gym.make('snake-v0')
+print("q to quit, wasd to move")
+env = gym.make('snake-v0')
 
-    done = False
-    rew = 0
-    obs = env.reset()
-    key = ""
-    action = "w"
-    while key != "q":
-        env.render()
-        key = input("action: ")
-        if key   == "w": action = 0
-        elif key == "d": action = 1
-        elif key == "s": action = 2
-        elif key == "a": action = 3
-        else: pass
-        obs, rew, done, info = env.step(action)
-        print("rew:", rew)
-        print("done:", done)
-        print("info")
-        for k in info.keys():
-            print("    ", k, ":", info[k])
-        if done:
-            obs = env.reset()
-    env.render(close=True)
+done = False
+reward = 0
+obs = env.reset()
+key = ""
+action = 0
+while key != "q":
+    env.render()
+    key = input("action: ")
+    if key   == "w": action = 0
+    elif key == "d": action = 1
+    elif key == "s": action = 2
+    elif key == "a": action = 3
+    else: pass
+    obs, reward, done, info = env.step(action)
+    print("reward:", reward)
+    print("done:", done)
+    print("info --")
+    for k in info.keys():
+        print("  ", k, ":", info[k])
+    if done:
+        obs = env.reset()
+env.render(close=True)

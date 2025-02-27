07f2b6a894ec7af9ee91009d8be7f7c204fe8146
Adam Peachey <u2101940@domain>
2025-02-27 19:30:27 +0000

    training

--- training/train.py
+++ training/train.py
@@ -5,44 +5,43 @@ import gym_snake
 import numpy as np
 import neat
 from neat import nn, population
+import matplotlib.pyplot as plt
 
 global pop
 rendering = True
 
-def look_to(direction, head, env):
-    grid = env.grid
+def look_to(env, direction):
     snake = env.controller.snakes[0]
-    height = env.grid_size[1]*env.unit_size
-    width = env.grid_size[0]*env.unit_size
+    if snake is None:
+       return 0
+    head = snake.head
+    grid = env.grid
 
     tail_found = False
     food_found = False
     
-    dist_food = width
-    dist_tail = width
+    dist_tail = env.grid_size[0] * env.unit_size
+    dist_food = env.grid_size[0] * env.unit_size
     dist = 0
 
-    while not grid.off_grid(head):
+    while not grid.off_grid(head) and not snake is None and not (tail_found and food_found):
       if not tail_found and grid.tail_space(head):
         tail_found = True
         dist_tail = dist
       if not food_found and grid.food_space(head):
         food_found = True
         dist_food = dist
-  
       dist += 1
-      if snake is None:
-        break
       head = snake.step(head, direction)
-    return dist_food
+  
+    return dist_tail
 
 def save_object(obj, filename):
     with open(filename, 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
 def load_object(filename):
-    with open(filename, 'rb') as f:
-        obj = pickle.load(f)
+    with open(filename, 'rb') as f: obj = pickle.load(f)
     return obj
 
 def save_best_instance(instance, filename='best_instances.pickle'):
@@ -53,61 +52,49 @@ def save_best_instance(instance, filename='best_instances.pickle'):
     save_object(instances, filename)
 
 def eval_fitness(genomes, config):
+   env = gym.make('snake-v0')
+   genome_number = 0
    best_instance = None
+   generation_number = 0
    best_fitness = 0
    best_foods = 0
-   genome_number = 0
-   generation_number = 0
    loop_punishment = 0.1
    near_food_score = 0.1
-   done = False
 
    for genome_id, g in genomes:
-      net = nn.FeedForwardNetwork.create(g, config) # create neural network
-      score = 0.0
+      net = nn.FeedForwardNetwork.create(g, config) 
+      score = 0
       hunger = 100 # 100 steps without eating and the snake dies
-
-      env = gym.make('snake-v0')
+      done = False
       env.reset()
-      if rendering:
-        env.render() 
+
       head = env.controller.snakes[0].head
-      dist_to_food = math.sqrt((head[0]-env.grid.food[0])**2 + (head[1]-env.grid.food[1])**2)
+      dist_to_food = abs((head[0]-env.grid.food[0]) + (head[1]-env.grid.food[1]))
 
       count = 0
-      error = 0
       pastPoints = set()
       foods = 0
 
       while not done:
         count += 1
-        outputs = net.activate([look_to(0, head, env), look_to(1, head, env), look_to(2, head, env), look_to(3, head, env)]) 
+        outputs = net.activate([look_to(env, 0), look_to(env, 1), look_to(env, 2), look_to(env, 3)]) 
         direction = outputs.index(max(outputs))
+        obs, reward, done, info = env.step(direction)
 
-        obs, rew, done, info = env.step(direction)
         hunger -= 1
-        if hunger <= 0:
-          break
+        if hunger <= 0: break
          
         if (head[0], head[1]) in pastPoints:
           score -= loop_punishment
         pastPoints.add((head[0], head[1]))
 
-        if rew > 0:
+        score += reward
+        if reward > 0:
           pastPoints = set()
           hunger += 100
           foods += 1
-          score += rew
-        else: 
-          if abs(head[0]-env.grid.food[0] + head[1]-env.grid.food[1]) <= 1:
-              score += near_food_score
-
-        if rendering:
-            env.render()
-      
-      if rendering:
-         print("score: ", score)
-         env.render()
+        elif dist_to_food <= 1:
+           score += near_food_score
       
       g.fitness = score 
       if best_instance is None or g.fitness > best_fitness:
@@ -122,11 +109,14 @@ def eval_fitness(genomes, config):
       best_fitness = max(best_fitness, g.fitness)
       print(f"Generation {generation_number} \tGenome {genome_number} \tFoods {foods} \tBF {best_foods} \tFitness {g.fitness} \tBest fitness {best_fitness} \tScore {score}")
       genome_number += 1
+  
    save_best_instance(best_instance)
    generation_number += 1
    if generation_number % 20 == 0:
       save_object(pop, 'population.dat')
       print('Exporting population')
+
+############################################################################################################
 
 config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                      neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -137,4 +127,9 @@ if len(sys.argv) > 1: # load population from file
     pop = load_object(sys.argv[1])
     print("Reading popolation from " + sys.argv[1])
 
-pop.run(eval_fitness, 10000) 
+pop.run(eval_fitness, 100) 
+
+best_instances = load_object('best_instances.pickle')
+best_instances = sorted(best_instances, key=lambda x: x['fitness'], reverse=True)
+print("Best instances: ", best_instances[:5])
+
